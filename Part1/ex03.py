214ef4b3da9000558f0b083c6eba251d4d3623c0
KaiXiang123795 <25073321@imail.sunway.edu.my>
2025-07-21 05:59:01 +0000

    haven done

---
+++ Part1/ex03.py
@@ -0,0 +1,8 @@
+string="Change Battery Faster"
+string_lower=string.lower()
+string_upper=string.upper()
+string_nospace=string.replace(" ", "")
+print(string)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
---
+++ Part1/ex04.py
@@ -0,0 +1,8 @@
+num1=200
+num2=4
+num3="60"
+num4="6"
+num3=int(num3)
+num4=int(num4)
+print(num1 / num2)
+print(num3 / num4)
