e5d5e0427637750fd96c44fd4351ce3c05c5c650
KaiXiang123795 <25073321@imail.sunway.edu.my>
2025-07-22 03:22:59 +0000

    Second day

---
+++ Part2/ex16.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+food = input("What is your favourite food? ")
+
+print(f"Hi {name}, I heard you love {food}!")
+
+What is your name? Marvin
+What is your favourite food? cendol
+Hi Marvin, I heard you love cendol!
